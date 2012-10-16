a5cf470dfeb5d9326328e7b919a132056351fa8a
Rolf van de Krol <info@rolfvandekrol.nl>
2012-10-16 10:03:02 +0200

    Make archiving work and introduce environment concept

--- kraftwagenrc.php
+++ kraftwagenrc.php
@@ -7,11 +7,12 @@
 
 // directory names
 $dirs = array(
-  'src', 'tools', 'build', // default dirs for normal development
-  'builds', // directory to keep track of all build, for production and staging
+  'src', 'tools', 'build', 'cnf', // default dirs for normal development
+  'builds', // directory to keep track of all builds, for production and staging
   'archive', // directory to save, for example, old make files
 
-  'src-tools' // tools directory in the src
+  'src-tools', // tools directory in the src
+  'src-cnf', // cnf directory in the src
 );
 foreach ($dirs as $dir) { 
   $options["{$dir}-dir"] = current(array_reverse(explode('-', $dir)));
@@ -23,6 +24,14 @@ $options += array(
   'make-file-tpl' => 'build.make.tpl',
   'date-pattern' => 'Ymd-His',
   'file-hashing-function' => 'sha1_file',
+
+  'environment-default' => 'production', // default environment is production for safety reasons
+  'environment-file' => 'environment',
+
+  'settings-file' => 'settings.php',
+  'settings-local-file' => 'settings.local.php',
+  'files-dir' => 'files',
+  'drupal-config-dir' => 'default',
 );
 
 // default way to find the Kraftwagen root directory is checking for the 
@@ -44,4 +53,15 @@ $options['update-commands'] = array_fill_keys(
     'kraftwagen-manifests',
   ),
   array('--yes')
+);
+
+$options['project-init-commands'] = array(
+  'kraftwagen-environment-setup' => array(),
+  'kraftwagen-environment-setup-settings' => array(),
+);
+
+$options['build-init-commands'] = array(
+  array('kraftwagen-build-symlink' => array($options['settings-file'])),
+  array('kraftwagen-build-symlink' => array($options['settings-local-file'])),
+  array('kraftwagen-build-symlink' => array($options['files-dir'])),
 );
